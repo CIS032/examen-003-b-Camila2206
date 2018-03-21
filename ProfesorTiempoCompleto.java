6a3aa6e79dfd51c74fb5b44290449267328d3931
Camila2206 <33434428+Camila2206@users.noreply.github.com>
2018-03-21 14:22:43 -0500

    Add files via upload

---
+++ ProfesorTiempoCompleto.java
@@ -0,0 +1,16 @@
+package Examen;
+
+
+import Examen.profesor;
+
+
+
+public class ProfesorTiempoCompleto extends profesor {
+    
+    private final int horas_Semanales = 40;
+
+    public int getHoras_Semanales() {
+        return horas_Semanales;
+    }
+
+}
---
+++ ProfesorTiempoParcial.java
@@ -0,0 +1,17 @@
+
+package Examen;
+
+
+public class ProfesorTiempoParcial extends Profesores {
+    
+    private int horasSemanales;
+
+    public int getHorasSemanales() {
+        return horasSemanales;
+    }
+
+    public void setHorasSemanales(int horasSemanales) {
+        this.horasSemanales = horasSemanales;
+    }
+
+}
